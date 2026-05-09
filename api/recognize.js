70567f3939691bd7335eb0087fcdc7512ee1ec71
kayaladream <136615856+kayaladream@users.noreply.github.com>
2026-05-10 00:08:03 +0800

    Update recognize.js

--- api/recognize.js
+++ api/recognize.js
@@ -88,7 +88,7 @@ export default async function handler(req, res) {
         },
         'PaddlePaddle/PaddleOCR-VL-1.5': {
           // 必须传入显式的指令，即使是尝试让它做全图识别
-          userText: 'Convert the document to markdown. Or OCR:', 
+          userText: '<image>\n请将图片中的文档内容转换为 Markdown 格式。', 
           temperature: 0.0, // OCR 任务必须降低到 0.0 保证确定性
           top_p: 1.0,
           frequency_penalty: 0.0 // 绝对不能开频率惩罚！
