f9bdbe6e32b47c2eda66d8d25f2ffad286de51de
kayaladream <136615856+kayaladream@users.noreply.github.com>
2026-05-13 08:48:54 +0800

    Remove loading message for null results
    Remove loading message when results are null.

--- src/App.js
+++ src/App.js
@@ -791,7 +791,6 @@ function App() {
         {showResultsSection && (
           <div className="result-section">
             <div className="result-container">
-              {isLoading && !currentIsStreaming && results[currentIndex] == null && <div className="loading result-loading">等待识别...</div>}
               {currentIsStreaming && (
                 <div className="result-text">
                   <div className="result-header"><span>第 {currentIndex + 1} 张图片的识别结果 (识别中...) {modelInfoText}</span></div>
